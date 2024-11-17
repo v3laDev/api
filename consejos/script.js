6a804952ba3f8ee2469d3684c037425203e3f899
v3laDev <27khero@proton.me>
2024-11-17 01:00:13 -0300

    API consejos

--- consejos/script.js
+++ consejos/script.js
@@ -1,14 +1,10 @@
-async function loadTip() {
-    try {
-      const response = await fetch('consejos.json');
-      const data = await response.json();
-      const tips = data.consejos;
-      const randomTip = tips[Math.floor(Math.random() * tips.length)];
-      document.getElementById('tip').innerText = randomTip;
-    } catch (error) {
-      document.getElementById('tip').innerText = 'Error loading the tip.';
-    }
-  }
-  
-  loadTip();
-  
+fetch('./consejos.json')
+  .then(response => response.json())
+  .then(data => {
+    const tips = data.tips;
+    const randomTip = tips[Math.floor(Math.random() * tips.length)];
+    document.write(randomTip);
+  })
+  .catch(() => {
+    document.write('Error');
+  });
